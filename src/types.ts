20efc19b1c32a32c537c39dc556e28abf14a9deb
101arrowz <arjunbarrett@gmail.com>
2022-12-03 20:15:18 -0800

    fix typings

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,12 @@
 export type CompressionFormat = 'gzip' | 'deflate' | 'deflate-raw';
 
-export interface CompressionStream extends TransformStream<Uint8Array, Uint8Array> {}
+export interface CompressionStream extends TransformStream<BufferSource, Uint8Array> {}
 export interface CompressionStreamConstructor {
   prototype: CompressionStream;
   new (format: CompressionFormat): CompressionStream;
 }
 
-export interface DecompressionStream extends TransformStream<Uint8Array, Uint8Array> {}
+export interface DecompressionStream extends TransformStream<BufferSource, Uint8Array> {}
 export interface DecompressionStreamConstructor {
   prototype: DecompressionStream;
   new (format: CompressionFormat): DecompressionStream;
